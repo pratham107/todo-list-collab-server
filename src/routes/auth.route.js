c5b8a5ecce49f175e2222c59af00751238e8acb7
prathmesh <pbhopale107@gmail.com>
2025-07-27 18:42:48 +0530

    bug fixes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -90,8 +90,8 @@ router.post('/login', async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true,
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .header("Authorization", `Bearer ${token}`)
